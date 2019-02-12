b96f97320421bba054f253efbf7e185730734476
jwoo7942 <47545607+jwoo7942@users.noreply.github.com>
2019-02-12 10:41:08 +0900

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+#blog/hello_times/10/ 으로 요청으면 10번 출력
+def hello_times(request, times):
+    message ="<font color = 'BLUE'>"
+    message = message + "안녕하세요?  " * times
+    message = message + "</font>"
+    return HttpResponse(message)
