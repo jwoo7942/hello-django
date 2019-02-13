0885be2fe4dd28aea9c48709c14bb77c06432feb
jwoo7942 <47545607+jwoo7942@users.noreply.github.com>
2019-02-13 11:08:01 +0900

    converts.py 추가

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -17,8 +17,21 @@ from django.contrib import admin
 from django.urls import path
 from blog.views import hello_times
 from blog.views import index, hello_times
+from blog.views import articles_by_year
+
+from django.urls import register_converter
+from blog.converters import FourDigitYearConverter
+
+register_converter(FourDigitYearConverter, 'year')
 
 urlpatterns = [
+    path('articles/<year:year>/', articles_by_year),
+
+    # re_path('^blog/1/$', post_detail),
+    # re_path('^blog/1/edit/$', post_edit),
+
+    path('', index),
+    # re_path(r'^$', index),
     path('admin/', admin.site.urls),
     path('blog/hello_times/<int:times>', hello_times),
     path('', index),
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+def articles_by_year(request, year):
+    return HttpResponse(f'''
+        {year}년도에 대한 목록
+    ''')
+
+    
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
