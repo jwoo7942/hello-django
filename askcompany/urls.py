c1d3278efd7ab4cf6590f90832e84e2ef565c7aa
jwoo7942 <47545607+jwoo7942@users.noreply.github.com>
2019-02-13 11:58:04 +0900

    update

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -14,25 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import hello_times
-from blog.views import index, hello_times
-from blog.views import articles_by_year
+from django.urls import include, path
 
-from django.urls import register_converter
-from blog.converters import FourDigitYearConverter
 
-register_converter(FourDigitYearConverter, 'year')
 
 urlpatterns = [
-    path('articles/<year:year>/', articles_by_year),
 
-    # re_path('^blog/1/$', post_detail),
-    # re_path('^blog/1/edit/$', post_edit),
-
-    path('', index),
-    # re_path(r'^$', index),
     path('admin/', admin.site.urls),
-    path('blog/hello_times/<int:times>', hello_times),
-    path('', index),
+    path('blog/', include('blog.urls')),
+    path('shop/', include('shop.urls')),
 ]
