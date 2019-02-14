4c42a99c0d038353f3f18bae31f5779e28d9763e
jwoo7942 <47545607+jwoo7942@users.noreply.github.com>
2019-02-14 19:23:16 +0900

    사원증이미지

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from blog.views import index, hello_times
 from blog.views import articles_by_year
 from blog.views import naver_realtime_keywords
 from blog.views import naver_blog_search
+from blog.views import 사원증_이미지_응답
 
 from django.urls import register_converter
 from blog.converters import FourDigitYearConverter
@@ -25,5 +26,7 @@ urlpatterns = [
     path('', index),
 
     path('naver/실시간검색어/', naver_realtime_keywords),
-    path('naver/네이버블로그검색/', naver_blog_search)
+    path('naver/네이버블로그검색/', naver_blog_search),
+
+    path('사원증/', 사원증_이미지_응답),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
 
 
 def articles_by_year(request, year):
@@ -9,10 +11,38 @@ def articles_by_year(request, year):
         {year}년도에 대한 목록
     ''')
 
-
-# Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+
+# 특정 Image에 이름,사번 새기기 위한 방법
+# 함수로 구현하는 방법
+# 함수로 구현될 걸 Class로 만드는 방법
+# 파이썬이 제공하는 Class를 이용하는 방법
+
+# def fn(template_name):
+#     def inner(request):
+#         return render(request, template_name)
+#     return inner
+
+# index = fn('blog/index.html')
+
+# index2 = fn('blog/index2.html')
+
+
+# class MyTemplateView:
+#     @classmethod
+#     def as_view(self, template_name):
+#         def inner(request):
+#             return render(request, template_name)
+#         return inner
+
+# index = MyTemplateView.as_view('blog/index.html')
+
+from django.views.generic import TemplateView
+
+index = TemplateView.as_view(template_name='blog/index.html')
+
 
 #blog/hello_times/10/ 으로 요청으면 10번 출력
 def hello_times(request, times):
@@ -65,3 +95,38 @@ def naver_blog_search(request):
 
     return HttpResponse(text)
 
+
+def 사원증_이미지_응답(request):
+    # ttf_path = 'C:/Windows/Fonts/malgun.ttf' # 윈도우, 맥:
+    # ttf_path = '/Library/Fonts/AppleGothic.ttf'
+
+    # FIXME: 각 운영체제에 맞게 경로를 설정해주세요.
+    # 파일을 복사한 후에, 경로 확인 후에 적용해주세요.
+    ttf_path = 'assets/fonts/arial.ttf'
+
+    text = request.GET.get('name', '익명')
+    # text = '이진석 (사번: 201900001)'
+
+    image_url = 'http://www.flowermeaning.com/flower-pics/Calla-Lily-Meaning.jpg'
+    res = requests.get(image_url) # 서버로 HTTP GET 요청하여, 응답 획득
+
+    io = BytesIO(res.content) # 응답의 Raw Body  메모리 파일 객체 BytesIO 인스턴스 생성
+    io.seek(0) # 파일의 처음으로 커서를 이동
+    canvas = Image.open(io).convert('RGBA') # 이미지 파일을 열고, RGBA 모드로 변환
+    font = ImageFont.truetype(ttf_path, 40) # 지정 경로의 TrueType 폰트, 폰트크기 40
+    draw = ImageDraw.Draw(canvas) # canvas에 대한 ImageDraw 객체 획득
+
+    left, top = 10, 10
+    margin = 10
+    width, height = font.getsize(text)
+    right = left + width + margin
+    bottom = top + height + margin
+
+    draw.rectangle((left, top, right, bottom), (255, 255, 224))
+    draw.text((15,15), text, font=font, fill=(20, 20, 20))
+
+    # canvas.show()  # 뷰에서는 필요가 없어서, 주석처리
+
+    response = HttpResponse(content_type='image/png')
+    canvas.save(response, format='PNG')    # HttpResponse 의 file-like 특성 활용
+    return response
