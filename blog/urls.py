55f8668ad8109fc150ebe08342247acd5b43b5b1
jwoo7942 <47545607+jwoo7942@users.noreply.github.com>
2019-02-14 09:11:08 +0900

    requests input

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,16 @@
-from django.urls import path
-from blog.views import hello_times
+from django.urls import path, re_path
 from blog.views import index, hello_times
 from blog.views import articles_by_year
+from blog.views import naver_realtime_keywords
 
 from django.urls import register_converter
 from blog.converters import FourDigitYearConverter
+from blog.converters import SlugUnicodeConverter
 
 register_converter(FourDigitYearConverter, 'year')
+register_converter(SlugUnicodeConverter, 'slug_unicode')
 
-app_nmae = 'blog'  
+app_nmae = 'blog'
 
 urlpatterns = [
     path('articles/<year:year>/', articles_by_year),
@@ -20,4 +22,6 @@ urlpatterns = [
     # re_path(r'^$', index),
     path('blog/hello_times/<int:times>', hello_times),
     path('', index),
+
+    path('naver/실시간검색어/', naver_realtime_keywords),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 
 def articles_by_year(request, year):
@@ -7,7 +9,7 @@ def articles_by_year(request, year):
         {year}년도에 대한 목록
     ''')
 
-    
+
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
@@ -18,3 +20,12 @@ def hello_times(request, times):
     message = message + "안녕하세요?  " * times
     message = message + "</font>"
     return HttpResponse(message)
+
+#Naver 실시간 검색 결과
+def naver_realtime_keywords(request):
+    res = requests.get("http://naver.com")
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    tag_list = soup.select('.PM_CL_realtimeKeyword_rolling .ah_k')
+    text = '<br/>\n'.join([tag.text for tag in tag_list])
+    return HttpResponse(text)
